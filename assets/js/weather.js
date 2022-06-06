20a2daf4c4980be84253d276c2f4a995e799b713
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-06-06 09:34:44 -0500

    layout done

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -11,6 +11,46 @@ var historyEl = document.getElementById("history");
 var FiveEl = document.getElementById("five-days");
 var weatherEl = document.getElementById("Weather-Today");
 var SearchingHist = JSON.parse(localStorage.getItem("search")) || [];
+//create forcast variable
+
+getForecast = function () {
+  var urlPoint =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=38.7267&lon=-9.1403&exclude=current,hourly,minutely,alerts&units=metric&appid={API key}";
+  var forecastTodayEl = document.getElementsByClassName("forecast");
+  //need to use fetch,promise that resolves response.
+  fetch(urlPoint)
+    .then(function (response) {
+      if (200 !== response.status) {
+        console.log(
+          "There is something wrong. Status Code: " + response.status
+        );
+        return;
+      }
+      forecastTodayEl[0].classList.add("loaded");
+      //json data and loop through the array
+      response.json().then(function (data) {
+        var weatherDay = "";
+        data.daily.forEach((value, index) => {
+          if (index > o) {
+            var name = new Date(value.dt * 1000).toLocaleDateString("en", {
+              weekday: "long",
+            });
+            var imageEl = value.weather[0].image;
+            var temperatureEl = value.temp.day.toFixed(0);
+            weatherDay = `<div class="forecast-day">
+                <p>${name}</p>
+                <p><span class="ima-${image}" title="${image}"></span></p>
+                <div class="forecast-day--temp">${temp}<sup>°C</sup></div>
+            </div>`;
+            forecastTodayEl[0].insertAdjacentHTML("before", weatherDay);
+          }
+        });
+      });
+    })
+    .catch(function (error) {
+      console.log("fetch Error :-S", error);
+    });
+};
 
 searchBtn.addEventListener("click", searchClick);
 function searchClick() {
